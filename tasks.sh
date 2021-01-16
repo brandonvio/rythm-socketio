25eade81b023b0a97f010e94a0d46fb0c8f2dd6e
Brandon Vicedomini <brandonvio@outlook.com>
2021-01-16 09:03:48 -0800

    Added tasks script.

--- tasks.sh
+++ tasks.sh
@@ -1,2 +1,5 @@
 aws ecs list-tasks --cluster rythm-cluster --service-name rythm-socketio-service
-aws ecs stop-task --cluster rythm-cluster --task arn:aws:ecs:us-west-2:778477161868:task/rythm-cluster/c2c3ecb055194d06ad4e2ea5b2ebd10f
+# aws ecs stop-task --cluster rythm-cluster --task arn:aws:ecs:us-west-2:778477161868:task/rythm-cluster/c2c3ecb055194d06ad4e2ea5b2ebd10f
+# aws ecs describe-tasks --cluster rythm-cluster --tasks arn:aws:ecs:us-west-2:778477161868:task/rythm-cluster/5cb2ca5c49af41cda538e5f6f72db950
+
+ecs-cli ps --cluster rythm-cluster
